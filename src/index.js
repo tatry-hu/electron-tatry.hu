65d02e3e25fa07cadc86a590c1d171a5807fea94
Szilard Orban <orbanszlrd@gmail.com>
2024-07-20 19:53:35 +0200

    Update eslint to version 9.7.0

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
-  // eslint-disable-line global-require
   app.quit();
 }
 
